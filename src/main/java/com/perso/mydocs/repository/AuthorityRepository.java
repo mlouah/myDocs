82bca30b1760e397970633f82569f8e4af23d029
louah <mlouah@s2m.ma>
2023-09-17 16:39:43 +0100

    Initial version of mydocs generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/perso/mydocs/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.perso.mydocs.repository;
+
+import com.perso.mydocs.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/perso/mydocs/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.perso.mydocs;
+
+import com.perso.mydocs.MydocsApp;
+import com.perso.mydocs.config.AsyncSyncConfiguration;
+import com.perso.mydocs.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { MydocsApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
